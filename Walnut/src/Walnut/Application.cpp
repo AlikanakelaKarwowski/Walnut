ba938e6c9157d832fdf9b49c0b77785017716b70
A.Karwowski <akk42@zips.uakron.edu>
2021-08-23 18:23:25 -0400

    create application (glclear not working)

--- Walnut/src/Walnut/Application.cpp
+++ Walnut/src/Walnut/Application.cpp
@@ -4,17 +4,20 @@
 
 #include "Walnut/Events/ApplicationEvent.h"
 #include "Walnut/Log.h"
-
+#include <GLFW/glfw3.h>
 namespace Walnut {
 
 	Application::Application() {
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 	Application::~Application() {
 
 	}
 	void Application::Run() {
-		WindowResizeEvent e(1280, 720);
-		WN_TRACE(e);
+		while (m_Running) {
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 }
--- Walnut/src/Walnut/Application.h
+++ Walnut/src/Walnut/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
 namespace Walnut {
 
 	class WALNUT_API Application
@@ -10,6 +11,9 @@ namespace Walnut {
 		virtual ~Application();
 
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//To Be defined in client
